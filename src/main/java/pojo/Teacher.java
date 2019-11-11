883a3caad81220c7f7adf02bf62ce001228c09d3
ad <529284652qq.com>
2019-11-11 15:38:35 +0800

    教师

---
+++ src/main/java/pojo/Teacher.java
@@ -0,0 +1,11 @@
+package pojo;
+
+/**
+ * Package: pojo
+ * Author: ad
+ * Date: Created in 2019/11/11 15:29
+ **/
+public class Teacher {
+    private String subject;//学科
+    private String name;//名字
+}
